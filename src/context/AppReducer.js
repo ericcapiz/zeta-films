e99a32e8f39c97f52b4b4bb49961545a066f9c48
ericcapiz <ericcapiz@gmail.com>
2021-03-25 14:43:02 -0500

    tweak

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,5 @@
 
-export default(state, action) => {
+const AppReducer =(state, action)=>{
     switch (action.type) {
         case "ADD_MOVIE_TO_WATCHLIST":
             return {
@@ -33,3 +33,4 @@ export default(state, action) => {
             return state;
     }
 }
+export default AppReducer;
