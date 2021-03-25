bb8f97e58f6be617db97b15cd17a0828715704f4
ericcapiz <ericcapiz@gmail.com>
2021-03-25 13:26:11 -0500

    data persist set up

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,9 +1,13 @@
-import {createContext, useReducer} from "react";
+import {createContext, useReducer, useEffect} from "react";
 import AppReducer from './AppReducer';
 
 const initialState = {
-    watchlist: [],
-    watched: [],
+
+    //checks for data in localstorage and will use that data/if nothing in local storge than the state is an empty array
+    //data persistant- app can refresh, close/stop, open/start and the data will still be within local storage
+    
+    watchlist: localStorage.getItem('watchlist') ? JSON.parse(localStorage.getItem('watchlist')) : [],
+    watched: localStorage.getItem('watched') ? JSON.parse(localStorage.getItem('watched')) : [],
 };
 
 export const GlobalContext = createContext(initialState);
@@ -12,6 +16,12 @@ export const GlobalContext = createContext(initialState);
 
 export const GlobalProvider = (props) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    //stores in local storage
+    useEffect (() => {
+        localStorage.setItem('watchlist', JSON.stringify(state.watchlist));
+        localStorage.setItem('watched', JSON.stringify(state.watched));
+    },[state]);
 
     //Actions
 
